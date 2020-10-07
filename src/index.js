d71411bc755e3577b066820165326ba3e9b00c20
tdev.js <tdev.js@gmail.com>
2020-10-07 18:05:26 +0530

    added redux with user to header and app component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import './index.css';
 
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
