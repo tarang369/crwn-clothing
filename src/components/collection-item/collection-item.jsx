e90140695c23f0cd7caf6e2709d413b7c57a85d5
tdev.js <tdev.js@gmail.com>
2020-10-08 16:38:17 +0530

    fixed shop data , cart utils to group item in cart

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,8 +1,12 @@
 import './collection-item.scss';
 
+import CustomButton from '../custom-button/custom-button';
 import React from 'react';
+import { addItem } from '../../redux/cart/cart.action';
+import { connect } from 'react-redux';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
   return (
     <div className='collection-item'>
       <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -10,8 +14,13 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
+      <CustomButton inverted onClick={() => addItem(item)}>
+        Add to Cart
+      </CustomButton>
     </div>
   );
 };
-
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item)),
+});
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 const CartActionTypes = {
   TOGGlE_CART_HIDDEN: 'TOGGlE_CART_HIDDEN',
+  ADD_ITEM: 'ADD_ITEM',
 };
 
 export default CartActionTypes;
