a2f2c3b1498d43dcf112e4b8aac322253cf1600a
Tarang Jain <29313637+tarang369@users.noreply.github.com>
2020-10-12 21:26:34 +0530

    added redux pattern to shop & directory

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import './index.css';
 
+import { persistor, store } from './redux/store';
+
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,20 @@
 import cartReducer from './cart/cart.reducer';
 import { combineReducers } from 'redux';
+import directoryReducer from './directory/directory.reducer';
+import { persistReducer } from 'redux-persist';
+import shopReducer from './shop/shop.reducer';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './user/user.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whiteList: ['cart'],
+};
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
+export default persistReducer(persistConfig, rootReducer);
