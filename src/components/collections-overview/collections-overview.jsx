c20d66d7e1c1e0f4cc47890bcd7985235ed8f216
Tarang Jain <29313637+tarang369@users.noreply.github.com>
2020-10-13 00:17:45 +0530

    bug

---
+++ src/components/collections-overview/collections-overview.jsx
@@ -0,0 +1,21 @@
+import './collections-overview.scss';
+
+import CollectionPreview from '../preview-collection/collection-preview';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
+
+const CollectionsOverview = ({ collections }) => {
+  return (
+    <div className='collection-overview'>
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview,
+});
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,18 +1,13 @@
-import CollectionPreview from '../../components/preview-collection/collection-preview';
+import CollectionPage from '../collection/collection';
+import CollectionsOverview from '../../components/collections-overview/collections-overview';
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selector';
+import { Route } from 'react-router-dom';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
   <div className='shop-page'>
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
